3961aa31a3386c5e7ed521648fff8231b439c9a6
MaksGovor <maksgovruha@gmail.com>
2021-05-16 13:58:16 +0300

    Add bad code

--- app/lib/worker.js
+++ app/lib/worker.js
@@ -1,3 +1,17 @@
 'use strict';
 
-module.exports = {};
+const transform = require('./transform');
+const v8 = require('v8');
+
+console.log('Run worker', process.pid);
+
+process.on('message', message => {
+  if (message.type === 'Buffer') console.log('Good');
+
+  const { data }= message;
+  console.log( data.length )
+  const result = transform(data);
+
+  const resultSerealized = v8.serialize(new Uint8ClampedArray(result));
+  process.send(resultSerealized);
+});
--- app/processing-core.js
+++ app/processing-core.js
@@ -1,13 +1,34 @@
 'use strict';
 
-const fs = require('fs');
-const fsp = fs.promises;
 const path = require('path');
-const worker = require('./lib/worker');
+const cp = require('child_process');
+const v8 = require('v8');
 const jimp = require('jimp');
 
 const filename = process.argv[2];
 const fullPath = path.resolve(__dirname, filename);
+const cpuCount = 1;
+const workers = [];
+const results = [];
+
+for (let i = 0; i < cpuCount; i++) {
+  const worker = cp.fork('./lib/worker.js');
+  console.log('Started worker:', worker.pid);
+  workers.push(worker);
+}
+
+workers.forEach((worker) => {
+  worker.on('message', message => {
+
+    console.log('Message from worker', worker.pid);
+
+    results.push(message.data)
+    if (results.length === cpuCount) {
+      console.log(results);
+      process.exit(0);
+    }
+  });
+});
 
 const get = (src, cb) => {
   jimp.read(src).then((img) => {
@@ -21,12 +42,17 @@ const get = (src, cb) => {
 
 get(fullPath, (err, imageData) => {
   if (err) throw err;
-  const data = imageData.data;
-  for (var i = 0; i < data.length; i += 4) {
-    data[i] += 30;
-    data[i + 1] += 30;
-    data[i + 2] += 30;
-  }
-  const buff = Buffer.from(data);
-  fsp.writeFile('res.jpg', buff);
+  
+  console.log(imageData.data);
+  const data = v8.serialize(imageData.data);
+  workers.forEach((worker) => {
+    worker.send(data);
+  })
 });
+
+const balancer = (imageData, countWorkers) => {
+  const { data, width, height } = imageData;
+  const blockSize = Math.floor(data.length / countWorkers);
+
+  
+}
