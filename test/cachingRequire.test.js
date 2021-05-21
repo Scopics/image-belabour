d8b6bee1ccd6a574ddc9aba37b9d43da1fffd870
MaksGovor <maksgovruha@gmail.com>
2021-05-21 18:36:46 +0300

    Add tests

--- test/cachingRequire.test.js
+++ test/cachingRequire.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const metatests = require('metatests');
+const path = require('path');
 const cachingRequire = require('../app/lib/utils/cachingRequire');
 
 metatests.test('test cachingRequire init valid ToPASS', (test) => {
@@ -17,5 +18,33 @@ metatests.test('test cachingRequire init non-valid ToTRHOW', (test) => {
   test.throws(() => cachingRequire(Number.MIN_VALUE));
   test.throws(() => cachingRequire(null));
   test.throws(() => cachingRequire(NaN));
+  test.end();
+});
+
+metatests.test('test cachingRequire get module', (test) => {
+  const crequire = cachingRequire();
+  const pathLib = path.resolve(__dirname, '../app/processing-core');
+  const pathNoExist = path.resolve(__dirname, 'bad-file.js');
+
+  const expected = require(pathLib);
+  const actual = crequire(pathLib);
+  const actualCache = crequire(pathLib);
+  const actualNoExistPath = crequire(pathNoExist);
+
+  test.strictSame(
+    actual,
+    expected,
+    'module not from the cache returns unexpected results'
+  );
+  test.strictSame(
+    actualCache,
+    expected,
+    'module from the cache returns unexpected results'
+  );
+  test.strictSame(
+    actualNoExistPath,
+    undefined,
+    'with a module that does not exist we should get undefined'
+  );
   test.end();
 });
